199e49e71eda7ae0821b264ccf3db3d41d48c563
Your Name <you@example.com>
2022-12-20 15:39:09 -0800

    task 1 and 2 redo

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """testing comments here"""
+
+
 class Square:
     """passing a class"""
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""square documentation"""
+
+
 class Square:
+    """square function"""
     def __init__(self, size):
         self.__size = size
