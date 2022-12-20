738ac1e5c0a5b9fc39b726d57428e20a880b49f4
Your Name <you@example.com>
2022-12-20 15:52:45 -0800

    task 3 revision

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 """square"""
+
+
 class Square:
     """Defines a square """
     def __init__(self, size=0):
-        if (isinstance(size, int) == False):
+        if not (isinstance(size, int)):
             raise TypeError('size must be an integer')
         if (size < 0):
             raise ValueError('size must be >= 0')
