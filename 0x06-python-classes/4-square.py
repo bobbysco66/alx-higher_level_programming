70dd6da03a65de0a775813d8d10d2e0439e32b19
Your Name <you@example.com>
2022-12-20 16:07:04 -0800

    task 5

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""square"""
+
+
+class Square:
+    """Defines a square """
+    def __init__(self, size=0):
+        if not (isinstance(size, int)):
+            raise TypeError('size must be an integer')
+        if (size < 0):
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
+
+    def area(self):
+        """square function"""
+        return (self.__size ** 2)
+
+    def size(self):
+        """square function"""
+        return (self.__size)
+
+    def size(self, value):
+        if not (isinstance(size, int)):
+            raise TypeError('size must be an integer')
+        if (size < 0):
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
